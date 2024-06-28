c1d8c78128b1d83e155904095eea2af99bcbdf24
JorderGomes <jordernoveau@gmail.com>
2024-06-28 19:01:02 -0300

    add form to search favors

--- src/app/components/pages/feed/feed.component.ts
+++ src/app/components/pages/feed/feed.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-feed',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrl: './feed.component.css'
 })
 export class FeedComponent {
+
+  searchForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.searchForm = new FormGroup({
+      tag: new FormControl('', [Validators.required]),
+      day: new FormControl('', [Validators.required]),
+      timeInit: new FormControl('', [Validators.required]),
+    });
+  }
+
+  get day() {
+    return this.searchForm.get('day')!;
+  }
+
+  get timeInit() {
+    return this.searchForm.get('timeInit')!;
+  }
+
+  get tag() {
+    return this.searchForm.get('tag')!;
+  }
+
+  searchFavor(formData: any, formDirective: FormGroupDirective): void {
+    if (this.searchForm.invalid) {
+      return;
+    }
+
+    console.log(this.searchForm.value);
+    console.log(formData);
+
+    formDirective.resetForm();
+    this.searchForm.reset();
+  }
+
 
 }
---
+++ src/app/interfaces/search-favor.ts
@@ -0,0 +1,6 @@
+export interface SearchFavor {
+    tag: string, 
+    day: string,
+    timeInit: string,
+    timeEnd: string,
+}
