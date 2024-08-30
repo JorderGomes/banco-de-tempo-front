cbde6a4a0c672fc2ee9da49a06db1c7072355ca6
JorderGomes <jordernoveau@gmail.com>
2024-08-30 11:27:08 -0300

    feat: update user card for row

--- src/app/services/user-card.service.ts
+++ src/app/services/user-card.service.ts
@@ -6,7 +6,20 @@ import { UserCardData } from '../interfaces/user-card-data';
 })
 export class UserCardService {
 
-  userCardList: UserCardData[] = [];
+  userCardList: UserCardData[] = [
+    {
+      urlUserImage: '../../../assets/ignore-assets/user-01.jpeg',
+      userName: 'Jane Smith',
+      talentName: 'Design',
+      schedules: new Map<string, number>([
+        ["segunda-10-14", 1],
+        ["quarta-14-18", 2],
+        ["sexta-9-12", 3],
+        ["domingo-10-13", 4]
+      ]),
+      qtdHours: 0
+    }
+  ];
 
   constructor() { }
 
