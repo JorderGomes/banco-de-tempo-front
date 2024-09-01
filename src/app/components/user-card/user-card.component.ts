fc94eabbacd0c26301fad0f630d586f7ab60694f
JorderGomes <jordernoveau@gmail.com>
2024-09-01 15:55:33 -0300

    feat: List schedules

--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -1,6 +1,9 @@
+// import { UserService } from '../../services/api/user.service';
+// import { UserCardData } from '../../interfaces/user-card-data';
+
 import { Component, Input } from '@angular/core';
-import { UserCardData } from '../../interfaces/user-card-data';
 import { Talent } from '../../interfaces/entities/talent';
+import { ScheduleService } from '../../services/api/schedule.service';
 
 @Component({
   selector: 'app-user-card',
@@ -8,7 +11,21 @@ import { Talent } from '../../interfaces/entities/talent';
   styleUrl: './user-card.component.css'
 })
 export class UserCardComponent {
-
+  
   @Input() talentData!: Talent;
+  
+  constructor(
+    public scheduleService: ScheduleService
+  ){}
+
+  getSchedules(userId: number) {
+    console.log(this.talentData);
+    if (!this.talentData.user.schedules){
+      this.scheduleService.searchSchedule(userId).subscribe((result) => {
+        console.log(result);
+        this.talentData.user.schedules = result;
+      });
+    }
+  }
 
 }
--- src/app/interfaces/entities/user.ts
+++ src/app/interfaces/entities/user.ts
@@ -1,7 +1,11 @@
+import { Schedule } from "./schedule";
+
 export interface User {
     id?: number,
     name: string,
     email: string,
     password: string,
-    balance: number
+    balance: number,
+    schedules?: Schedule[],
+    schedulesString?: string[]
 }
